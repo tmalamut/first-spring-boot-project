bd22d6ca1d364fa5fbe0d3fd4aba47d0b8d7055b
Taylor Malamut <tfmalamut@coastal.edu>
2021-06-17 01:57:45 -0400

    Try again

--- src/main/java/com/example/springboot/student/StudentConfig.java
+++ src/main/java/com/example/springboot/student/StudentConfig.java
@@ -1,2 +1,34 @@
-package com.example.springboot.student;public class StudentConfig {
+package com.example.springboot.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student willow = new Student(
+                    "Willow",
+                    "willow.dog@gmail.com",
+                    LocalDate.of(2014, Month.SEPTEMBER, 2)
+            );
+
+            Student gnash = new Student(
+                    "Gnash",
+                    "gnash.nhl@gmail.com",
+                    LocalDate.of(2004, Month.SEPTEMBER, 2)
+            );
+
+            repository.saveAll(
+                    List.of(willow, gnash)
+            );
+        };
+    }
 }
--- src/main/java/com/example/springboot/student/StudentService.java
+++ src/main/java/com/example/springboot/student/StudentService.java
@@ -1,2 +1,63 @@
-package com.example.springboot.student;public class StudentService {
+package com.example.springboot.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email Taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("student with id" + studentId + " does not exist."));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email Taken");
+            }
+            student.setEmail(email);
+        }
+
+
+    }
 }
